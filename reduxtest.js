e1a6625ad5f454437fffef21bbcf57ae70bbcffa
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-06-26 23:12:14 +0530

    age value substraction

--- reduxtest.js
+++ reduxtest.js
@@ -10,11 +10,22 @@ const myReducer = (state = initialState, action) => {
     if(action.type === 'ADD'){
         newState.age += 1;
     }
+
+    if(action.type === 'SUBSTRACT'){
+        newState.age -= 1;
+    }
+
     return newState;
 }
 
 const store = createStore(myReducer);
 
+console.log('initial state' + JSON.stringify(store.getState()));
+
 store.dispatch({type:'ADD'});
 
-console.log(store.getState());
+console.log('after add' + JSON.stringify(store.getState()));
+
+store.dispatch({type:'SUBSTRACT'});
+
+console.log('after substract' + JSON.stringify(store.getState()));
